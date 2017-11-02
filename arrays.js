269a5af51851d99abd3ce77141dcbbd59469c7c0
Matthew Robinson <robinson.me@gmail.com>
2017-11-02 22:43:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,12 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray (a, n) {
+  var a = array
+  var n = n
+  a.unshift(n)
+  return a
+}
+
+function destructivelyAddElementToEndOfArray() {
+  
+}
