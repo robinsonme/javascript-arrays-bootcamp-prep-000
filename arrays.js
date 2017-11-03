616621bfb0ef3971fe6098d6c05c41c901439b0c
Matthew Robinson <robinson.me@gmail.com>
2017-11-03 00:10:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,12 @@ function destructivelyAddElementToEndOfArray(array, element) {
   array.unshift(element)
   return array
 }
+
+function addElementToEndOfArray (array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
